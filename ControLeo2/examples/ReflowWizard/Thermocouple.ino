bfdf6c873cae7902980764daa858413b6ac17870
Julius Lehmann <jlkon.jl@gmail.com>
2016-12-06 19:48:41 +0100

    Improved temperature readings

--- ControLeo2/examples/ReflowWizard/Thermocouple.ino
+++ ControLeo2/examples/ReflowWizard/Thermocouple.ino
@@ -5,7 +5,7 @@
 // takeCurrentThermocoupleReading() is called from the Timer 1 interrupt (see "Servo" tab).  It is
 // called 5 times per second.
 
-#define NUM_READINGS           2   // Number of readings to average the temperature over (5 readings = 1 second)
+#define NUM_READINGS           3   // Number of readings to average the temperature over (5 readings = 1 second)
 #define ERROR_THRESHOLD        15  // Number of consecutive faults before a fault is returned
 
 
